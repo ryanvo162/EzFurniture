c4604cd85c4a807106e1f23acf52757f6bffa867
_thuan01_ <76669619+quangthuan01@users.noreply.github.com>
2022-03-31 20:43:26 +0700

    ComfirmOrderScreen

--- screens/main.js
+++ screens/main.js
@@ -14,12 +14,14 @@ import OTPScreen from "./OTPScreen/otp";
 import DetailScreen from "./DetailScreen/detail";
 import VoucherScreen from "./VoucherScreen/voucher";
 import OrderScreen from "./OrderScreen/order";
+import ComfirmOrderScreen from "./ComfirmOrderScreen";
+
 
 export default function AppProvider() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer style={styles.container}>
-      <Stack.Navigator initialRouteName="HomeScreen">
+      <Stack.Navigator initialRouteName="ComfirmOrderScreen">
         <Stack.Screen
           name="SlashScreen"
           component={SlashScreen}
@@ -77,6 +79,12 @@ export default function AppProvider() {
           component={VoucherScreen}
           options={{ headerShown: false }}
         />
+        <Stack.Screen
+          name="ComfirmOrderScreen"
+          component={ComfirmOrderScreen}
+          options={{ headerShown: false }}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
