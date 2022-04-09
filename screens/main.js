264a1b339bbaf82fd9d54bf2c56dd874e5fbf16b
Ryan Võ <71304811+ryanvo162@users.noreply.github.com>
2022-04-09 08:53:08 +0700

    Update main.js

--- screens/main.js
+++ screens/main.js
@@ -26,7 +26,7 @@ export default function AppProvider() {
   return (
     <NavigationContainer style={styles.container}>
       <Stack.Navigator
-        initialRouteName="DeliveryAddressScreen"
+        initialRouteName="SlashScreen"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="SlashScreen" component={SlashScreen} />
